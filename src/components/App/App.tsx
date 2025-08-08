f073ed20961a092d481c09f17ca9c6767df234b9
Borys Savenok <horosh.narod@gmail.com>
2025-08-08 03:06:01 +0300

    home work 2

---
+++ src/components/App/App.tsx
@@ -0,0 +1,56 @@
+// import { useState } from 'react'
+import css from './App.module.css';
+import CafeInfo from '../CafeInfo/Cafeinfo';
+import { useState } from 'react';
+import type { Votes } from '../types/Votes';
+import type { VoteType } from '../types/Votes';
+import VoteOptions from '../VoteOptions/VoteOptions';
+import VoteStats from '../VoteStats/VoteStats';
+import Notification from '../Notification/Notification';
+export default function App() {
+  const [counts, setCounts] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+  const handleVote = (type: VoteType) => {
+    setCounts({
+      ...counts,
+      [type]: counts[type] + 1,
+    });
+  };
+  const resetVotes = () => {
+    setCounts({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+  const totalVotes = Object.values(counts).reduce(
+    (sum, value) => sum + value,
+    0
+  );
+  const positiveRate = totalVotes
+    ? Math.round((counts.good / totalVotes) * 100)
+    : 0;
+  const isVisible = totalVotes ? true : false;
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={isVisible}
+      />
+      {totalVotes ? (
+        <VoteStats
+          votes={counts}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+}
