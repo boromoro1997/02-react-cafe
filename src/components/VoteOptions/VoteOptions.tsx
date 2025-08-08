f594027ed24d0b534edb50c523c17b469a197b5f
Borys Savenok <horosh.narod@gmail.com>
2025-08-08 03:38:01 +0300

    fixed mistakes

--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -1,5 +1,5 @@
 import css from './VoteOptions.module.css';
-import type { VoteType } from '../../types/Votes';
+import type { VoteType } from '../../types/votes';
 interface voteCounterProps {
   onVote: (vote: VoteType) => void;
   onReset: () => void;
